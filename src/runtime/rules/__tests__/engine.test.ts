180bd505197cc2c0f737d53339f250e9844927d8
Zac Rosenbauer <zacrosenbauer@gmail.com>
2021-05-02 13:16:20 -0500

    fix tests

--- src/runtime/rules/__tests__/engine.test.ts
+++ src/runtime/rules/__tests__/engine.test.ts
@@ -28,9 +28,9 @@ describe('operators', () => {
   });
 });
 
-describe('buildRules', () => {
+describe('transformActionToRule', () => {
   it('properly creates rule from action', () => {
-    expect(engine.buildRules([
+    expect(engine.transformActionToRule(
       {
         type: 'query',
         id: '123',
@@ -42,45 +42,43 @@ describe('buildRules', () => {
           }
         }
       }
-    ])).toEqual([
-      {
-        conditions: conditions,
-        event: {
-          type: 'assertion',
-          params: {
-            foo: true
-          }
+    )).toEqual({
+      conditions: conditions,
+      event: {
+        type: 'assertion',
+        params: {
+          foo: true
         }
       }
-    ]);
+    });
   });
 
-  it('ignores actions without conditions', () => {
-    expect(engine.buildRules([
-      {
-        type: 'click',
-        id: '123',
-        selector: 'h1'
-      }
-    ])).toEqual([]);
+  it('returns null if action does not have conditions', () => {
+    expect(engine.transformActionToRule(      {
+      type: 'click',
+      id: '123',
+      selector: 'h1'
+    })).toEqual(null);
   });
 });
 
 describe('adds rules', () => {
   it('adds the proper rules', async () => {
-    const eng = engine.create([
-      {
-        type: 'query',
-        id: '123',
-        selector: 'h1',
-        conditions: {
-          ...conditions,
-          params: {
-            foo: true
+    const eng = engine.create(_.compact([
+      engine.transformActionToRule(
+        {
+          type: 'query',
+          id: '123',
+          selector: 'h1',
+          conditions: {
+            ...conditions,
+            params: {
+              foo: true
+            }
           }
         }
-      }
-    ]);
+      )
+    ]));
 
     const result = await eng.run({ aNumber: 1 });
     expect(result.events[0]).toEqual({
